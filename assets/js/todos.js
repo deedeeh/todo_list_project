2c2e734cc10dbd90c208c25cfe13e04ff369204b
deedeeh <dinahafez.8@gmail.com>
2018-09-01 19:15:07 +0100

    Delete todos using parent(), fadeOut() & remove() methods - part2

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,15 @@
-// Check off specific todos by clicking
+//Check off specific todos by clicking
 $("li").click(function(){
   //one line of code by creating a class in css and use toggleClass method
   $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("span").click(function(e){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  e.stopPropagation();
 });
 
 //The harder & longer way of doing it
